19151684bf4f8f4d6ce98e83e920b4567ba096f4
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-08-13 11:05:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ToggreMuter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appList = new System.Windows.Forms.ListBox();
+            this.testlabel = new System.Windows.Forms.Label();
+            this.labelProcessList = new System.Windows.Forms.Label();
+            this.IgnoredList = new System.Windows.Forms.ListBox();
+            this.movButton = new System.Windows.Forms.Button();
+            this.muteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // appList
+            // 
+            this.appList.FormattingEnabled = true;
+            this.appList.ItemHeight = 12;
+            this.appList.Location = new System.Drawing.Point(12, 66);
+            this.appList.Name = "appList";
+            this.appList.Size = new System.Drawing.Size(558, 472);
+            this.appList.TabIndex = 0;
+            this.appList.SelectedIndexChanged += new System.EventHandler(this.appList_SelectedIndexChanged);
+            // 
+            // testlabel
+            // 
+            this.testlabel.AutoSize = true;
+            this.testlabel.Location = new System.Drawing.Point(455, 51);
+            this.testlabel.Name = "testlabel";
+            this.testlabel.Size = new System.Drawing.Size(49, 12);
+            this.testlabel.TabIndex = 1;
+            this.testlabel.Text = "testlabel";
+            // 
+            // labelProcessList
+            // 
+            this.labelProcessList.AutoSize = true;
+            this.labelProcessList.Location = new System.Drawing.Point(12, 51);
+            this.labelProcessList.Name = "labelProcessList";
+            this.labelProcessList.Size = new System.Drawing.Size(66, 12);
+            this.labelProcessList.TabIndex = 2;
+            this.labelProcessList.Text = "プロセス一覧";
+            // 
+            // IgnoredList
+            // 
+            this.IgnoredList.FormattingEnabled = true;
+            this.IgnoredList.ItemHeight = 12;
+            this.IgnoredList.Location = new System.Drawing.Point(738, 306);
+            this.IgnoredList.Name = "IgnoredList";
+            this.IgnoredList.Size = new System.Drawing.Size(197, 232);
+            this.IgnoredList.TabIndex = 3;
+            // 
+            // movButton
+            // 
+            this.movButton.Location = new System.Drawing.Point(614, 251);
+            this.movButton.Name = "movButton";
+            this.movButton.Size = new System.Drawing.Size(75, 23);
+            this.movButton.TabIndex = 4;
+            this.movButton.Text = "追加/削除";
+            this.movButton.UseVisualStyleBackColor = true;
+            this.movButton.Click += new System.EventHandler(this.movButton_Click);
+            // 
+            // muteButton
+            // 
+            this.muteButton.Location = new System.Drawing.Point(614, 185);
+            this.muteButton.Name = "muteButton";
+            this.muteButton.Size = new System.Drawing.Size(75, 23);
+            this.muteButton.TabIndex = 5;
+            this.muteButton.Text = "ミュート";
+            this.muteButton.UseVisualStyleBackColor = true;
+            this.muteButton.Click += new System.EventHandler(this.muteButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 558);
+            this.Controls.Add(this.muteButton);
+            this.Controls.Add(this.movButton);
+            this.Controls.Add(this.IgnoredList);
+            this.Controls.Add(this.labelProcessList);
+            this.Controls.Add(this.testlabel);
+            this.Controls.Add(this.appList);
+            this.Name = "MainForm";
+            this.Text = "ToggleMuter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox appList;
+        private System.Windows.Forms.Label testlabel;
+        private System.Windows.Forms.Label labelProcessList;
+        private System.Windows.Forms.ListBox IgnoredList;
+        private System.Windows.Forms.Button movButton;
+        private System.Windows.Forms.Button muteButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace ToggreMuter
+{
+    public partial class MainForm : Form
+    {
+        private bool isMuted = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // AudioSessionManagerをインスタンス化してプロセス情報を取得
+            AudioSessionManager audioSessionManager = new AudioSessionManager();
+            ProcessInfo[] processInfoList = audioSessionManager.GetProcessInfoList();
+
+            // プロセス情報を辞書型に追加
+            Dictionary<int, string> processDictionary = new Dictionary<int, string>();
+            if (processInfoList != null)
+            {
+                foreach (var processInfo in processInfoList)
+                {
+                    // プロセスIDとプロセス名を辞書型に追加
+                    processDictionary.Add(processInfo.ProcessId, processInfo.ProcessName);
+
+                    // プロセス名だけをlistBoxに追加
+                    appList.Items.Add(processInfo.ProcessName+"("+ processInfo.ProcessId+")");
+                }
+            }
+
+            // 選択されたアイテムの変更イベントをハンドル
+            appList.SelectedIndexChanged += appList_SelectedIndexChanged;
+
+            // 辞書型をTagプロパティに設定して保持
+            appList.Tag = processDictionary;
+        }
+
+        private void appList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Dictionary<int, string> processDictionary = (Dictionary<int, string>)appList.Tag;
+            int processId = getSelectedProcessId();
+            string processName = processDictionary[processId];
+
+            testlabel.Text = "プロセス名: " + processName + ", プロセスID: " + processId.ToString();
+        }
+
+        private void movButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> processDictionary = (Dictionary<int, string>)appList.Tag;
+            int processId = getSelectedProcessId();
+            string processName = processDictionary[processId];
+            String listText = processName + "(" + processId + ")";
+
+            if (!IgnoredList.Items.Contains(listText))
+            {
+                IgnoredList.Items.Add(listText);
+            }
+            else
+            {
+                IgnoredList.Items.Remove(listText);
+            }
+        }
+
+        private int getSelectedProcessId()
+        {
+            if (appList.SelectedItems.Count > 0)
+            {
+                int selectedIndex = appList.SelectedIndex;
+                Dictionary<int, string> processDictionary = (Dictionary<int, string>)appList.Tag;
+
+                // 選択されたアイテムからプロセスIDを取得し、辞書型からプロセス名を取得
+                int processId = (int)processDictionary.Keys.ElementAt(selectedIndex);
+                return processId;
+            }
+            return -1;
+        }
+
+        private void muteButton_Click(object sender, EventArgs e)
+        {
+            isMuted = !isMuted;
+            muteButton.Text = isMuted ? "ミュート解除" : "ミュート";
+            List<int> IgnoredProcessIDs = new List<int>();
+            foreach (String listText in IgnoredList.Items)
+            {
+                int start = listText.IndexOf("(") + 1;
+                int end = listText.IndexOf(")");
+                String processID = listText.Substring(start, end - start);
+                IgnoredProcessIDs.Add(Int32.Parse(processID));
+            }
+
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDevice defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            var sessions = defaultDevice.AudioSessionManager.Sessions;
+            for (int i = 0; i < sessions.Count; i++)
+            {
+                var session = sessions[i];
+                if (!IgnoredProcessIDs.Contains(Convert.ToInt32(session.GetProcessID)))
+                {
+                    session.SimpleAudioVolume.Mute = !session.SimpleAudioVolume.Mute;
+                }
+            }
+        }
+    }
+
+    public class AudioSessionManager
+    {
+        private MMDeviceEnumerator deviceEnumerator;
+        private MMDevice defaultPlaybackDevice;
+
+        public AudioSessionManager()
+        {
+            deviceEnumerator = new MMDeviceEnumerator();
+            defaultPlaybackDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+        }
+
+        public ProcessInfo[] GetProcessInfoList()
+        {
+            if (defaultPlaybackDevice != null)
+            {
+                var sessionManager = defaultPlaybackDevice.AudioSessionManager;
+                var sessions = sessionManager.Sessions;
+
+                ProcessInfo[] processInfoList = new ProcessInfo[sessions.Count];
+                for (int i = 0; i < sessions.Count; i++)
+                {
+                    var session = sessions[i];
+                    var processId = session.GetProcessID;
+                    var process = System.Diagnostics.Process.GetProcessById((int)processId);
+
+                    processInfoList[i] = new ProcessInfo
+                    {
+                        ProcessId = (int)processId,
+                        ProcessName = process.ProcessName
+                    };
+                }
+
+                return processInfoList;
+            }
+
+            return null;
+        }
+    }
+    public class ProcessInfo
+    {
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
+    }
+}
