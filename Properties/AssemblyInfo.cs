2b4ed80aea31f054c0078be5d9c9ce50101b01a3
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-09-25 20:33:38 +0900

    refresh toggleMuter -> ToggleMuter

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般的な情報は、次の方法で制御されます
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("toggleMuter")]
+[assembly: AssemblyTitle("ToggleMuter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("toggleMuter")]
+[assembly: AssemblyProduct("ToggleMuter")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
